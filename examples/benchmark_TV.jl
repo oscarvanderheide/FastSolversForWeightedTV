1797804866d8e0cbf1cfabcf064c5397c359b292
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2021-10-07 18:00:17 +0200

    Small fix

--- examples/benchmark_TV.jl
+++ examples/benchmark_TV.jl
@@ -12,14 +12,11 @@ n = (256, 256)
 y_orig = Float32.(TestImages.shepp_logan(n...)) |> gpu
 y_orig = y_orig/norm(y_orig, Inf)
 y = y_orig+gpu(0.1f0*randn(Float32, n))
-ε = 0.9f0*g(y)
 
-# Optimization options
-opt = opt_fista(; steplength=1f0/8f0, niter=10000, tol_x=nothing, nesterov=true)
-
-# # Proxy
-# λ = 0.5f0*norm(y)^2/g(y)
-# @btime proxy(y, λ, g, opt);
+# Proxy
+λ = 0.5f0*norm(y)^2/g(y)
+@btime proxy(y, λ, g, opt);
 
 # Projection test
+ε = 0.9f0*g(y)
 @btime project(y, ε, g, opt);

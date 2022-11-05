b4529a89c2fa3bbcda03fa479f89e6260cbca55c
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2022-11-05 12:58:28 +0100

    Fixed grad+indicator issue

--- test/test_tv_norm.jl
+++ test/test_tv_norm.jl
@@ -5,9 +5,8 @@ CUDA.allowscalar(false)
 T = Float64
 n1d = 16
 nc = 3
-nb = 16
-step = T(1e-6)
-rtol = T(1e-3)
+step = T(1e-5)
+rtol = T(1e-2)
 
 for dim = 1:3, flag_gpu = [false], is_complex = [true, false]
     
@@ -22,18 +21,22 @@ for dim = 1:3, flag_gpu = [false], is_complex = [true, false]
     y = randn(CT, n); flag_gpu && (y = CT.(y |> gpu))
     g = gradient_norm(2, 1, n, h; complex=is_complex, optimizer=opt); flag_gpu && (g = g |> gpu)
 
+    # Constraint
+    C = zero_set(CT, randn(T, size(y)).>0)
+
     # Gradient test (proxy)
     λ = T(0.5)*norm(y)^2/g(y)
-    fun = proxy_objfun(g, λ)
+    fun = proxy_objfun(g+indicator(C), λ)
     @test test_grad(fun, y; step=step, rtol=rtol)
 
     # Projection test
-    ε = T(0.8)*g(y)
-    x = project(y, ε, g)
-    @test g(x) ≈ ε rtol=rtol
+    ε = T(0.1)*g(y)
+    x = project(y, ε, g+indicator(C))
+    @test (g(x) ≤ ε) || isapprox(g(x), ε; rtol=rtol)
+    @test x ∈ C
 
     # Gradient test (projection)
-    fun = proj_objfun(g, ε)
+    fun = proj_objfun(g+indicator(C), ε)
     @test test_grad(fun, y; step=step, rtol=rtol)
 
 end
